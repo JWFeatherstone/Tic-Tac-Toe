09fa719d9cf709285f0dc9a759f68d86111f650c
John Featherstone <johnwfeatherstone.personal@gmail.com>
2023-03-06 13:13:46 -0700

    Simplify checkForWin function by creating array of arrays for win conditions and using destructuring assignment to create a streamlined for loop.

--- game.js
+++ game.js
@@ -44,28 +44,23 @@ class Game {
   };
 
   checkForWin() {
-    var columnOne = [this.board[0], this.board[3], this.board[6]];
-    var columnTwo = [this.board[1], this.board[4], this.board[7]];
-    var columnThree = [this.board[2], this.board[5], this.board[8]];
-    var rowOne = [this.board[0], this.board[1], this.board[2]];
-    var rowTwo = [this.board[3], this.board[4], this.board[5]];
-    var rowThree = [this.board[6], this.board[7], this.board[8]];
-    var diagonalOne = [this.board[0], this.board[4], this.board[8]];
-    var diagonalTwo = [this.board[2], this.board[4], this.board[6]];
-    var winConditions = [columnOne, columnTwo, columnThree, rowOne, rowTwo, rowThree, diagonalOne, diagonalTwo];
+    var winConditions = [
+        [0,1,2], [3,4,5], [6,7,8], 
+        [0,3,6], [1,4,7], [2,5,8], 
+        [0,4,8], [2,4,6]
+    ];
     for (var i = 0; i < winConditions.length; i++) {
-      var sum = winConditions[i].reduce(function(a, b) {
-        return a + b;
-      });
+      var [a, b, c] = winConditions[i];
+      var board = this.board;
 
-      if (sum == 3) {
+      if (board[a] === 1 && board[a] === board[b] && board[a] === board[c]) {
         this.playerOne.increaseWins();
         this.winner = 'Player One'
         updateAnnouncement(this.winner);
         setTimeout(this.resetGame, 2000);
       } 
     
-      else if (sum == 12) {
+      else if (board[a] === 4 && board[a] === board[b] && board[a] === board[c]) {
         this.playerTwo.increaseWins();
         this.winner = 'Player Two'
         updateAnnouncement(this.winner);
